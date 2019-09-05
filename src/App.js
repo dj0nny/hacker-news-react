12ee202e7af0d4b62b4fc1a1ba289c7d3d646d36
dj0nny <frank96990@gmail.com>
2019-09-05 15:23:44 +0200

    add Header and News components, set up routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/Header';
+import News from './components/News';
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="app">
+          <Header />
+          <Route exact path="/" component={News} />
         </div>
       </Router>
     )
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <div className="header">
+       <div className="logo">
+        <img src="https://news.ycombinator.com/y18.gif" />
+        <span className="title">Hacker news</span>
+      </div>
+      <div className="menu-container">
+        <ul id="menu">
+          <li className="item"><Link to="/">news</Link></li> |
+          <li className="item"><Link to="/newest">newest</Link></li> |
+          <li className="item"><Link to="/ask">ask</Link></li> |
+          <li className="item"><Link to="/show">Home</Link></li> |
+          <li className="item"><Link to="/jobs">jobs</Link></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
