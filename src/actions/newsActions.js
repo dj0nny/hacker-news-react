4534172050e70acc68fbe28b087f81957b78c5d4
dj0nny <frank96990@gmail.com>
2019-09-26 15:29:11 +0200

    add fetchNews action

--- src/actions/newsActions.js
+++ src/actions/newsActions.js
@@ -1,5 +1,12 @@
 import { FETCH_NEWS } from './types';
 
+const BASE_URL = 'https://api.hackernews.io';
+
 export const fetchNews = () => dispatch => {
-  
+  fetch(`${BASE_URL}/news`)
+    .then(res => res.json())
+    .then(data => dispatch({
+      type: FETCH_NEWS,
+      payload: data
+    }));
 };
